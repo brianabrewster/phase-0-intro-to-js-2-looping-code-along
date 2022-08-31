94fdafc6917388ffed3775299994a83f936ee9d0
Briana <briana_brewster4@yahoo.com>
2022-08-31 09:18:49 -0500

    commit tree d7241fcec556a8a36b00e4fefcb1ec852092b895

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+for (let age = 30; age <40; age++) {
+    console.log(`I'm ${age} years old. Happy Birthday to me!`);
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+wrapGifts(gifts);
+
+const names = ["John", "Carly", "Steven"]
+const messages = []
+function writeCards(names, surprise) {
+for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+}
+return messages
+}
+
+function countDown() {
+    let number = 10
+    while (number > -1) {
+        console.log(number--)
+    }
+}
